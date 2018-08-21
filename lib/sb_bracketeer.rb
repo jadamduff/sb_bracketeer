8b8d21a410f3fa189fd5c081f15e13c048ce716b
Adam Duff <j.adam.duff@gmail.com>
2018-08-21 02:30:39 +0000

    Bracket loads shell rounds.

--- lib/sb_bracketeer.rb
+++ lib/sb_bracketeer.rb
@@ -1,2 +1,7 @@
 require_relative "sb_bracketeer/version"
 require_relative 'sb_bracketeer/cli'
+require_relative 'sb_bracketeer/bracket'
+require_relative 'sb_bracketeer/game'
+require_relative 'sb_bracketeer/round'
+require_relative 'sb_bracketeer/scraper'
+require_relative 'sb_bracketeer/team'
---
+++ lib/sb_bracketeer/round.rb
@@ -0,0 +1,21 @@
+class SbBracketeer::Round
+  attr_accessor :type, :num_games, :game1, :game2, :game3, :game4
+  def initialize(type)
+    @type = type
+  end
+
+  def self.create_by_year_and_type(year, type)
+    round = self.new(type)
+    case round.type
+    when "wildcard_round"
+      round.num_games = 4
+    when "divisional_round"
+      round.num_games = 4
+    when "conference_championship"
+      round.num_games = 2
+    when "super_bowl"
+      round.num_games = 1
+    end
+    round
+  end
+end

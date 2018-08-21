24e028a5e649f1db64840cee21b51c22af10026f
Adam Duff <j.adam.duff@gmail.com>
2018-08-21 01:18:30 +0000

    Added shell Bracket class

---
+++ lib/sb_bracketeer/bracket.rb
@@ -0,0 +1,9 @@
+class SbBracketeer::Bracket
+  attr_accessor :year, :wildcard_round, :divisional_round, :conference_championship, :super_bowl
+
+  def initialize(year)
+    @year = year
+  end
+
+  
+end

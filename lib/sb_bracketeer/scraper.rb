abcc69954420003b5a583911835289ce8521a40e
Adam Duff <j.adam.duff@gmail.com>
2018-08-27 23:05:39 +0000

    Created @data instance variable for Scraper class to prevent data from being scraped multiple times.

--- lib/sb_bracketeer/scraper.rb
+++ lib/sb_bracketeer/scraper.rb
@@ -5,15 +5,16 @@ class SbBracketeer::Scraper
   # Loads an array of html data from a specified url.
   def self.load(url)
     doc = Nokogiri::HTML(open(url))
-    doc
   end
 
   def self.load_bracket_data(year)
-    data = SbBracketeer::Scraper.load('https://www.footballdb.com/seasons/nfl/' + year.to_s)
-    data.css("table.statistics.scrollable tbody tr.left")
+
+    @data ||= (puts "****** Scraping Bracket"; SbBracketeer::Scraper.load('https://www.footballdb.com/seasons/nfl/' + year.to_s))
+    @data.css("table.statistics.scrollable tbody tr.left")
   end
 
   def self.load_roster(team, year)
+    puts "****** Scraping Roster"
     data = SbBracketeer::Scraper.load('https://www.footballdb.com/teams/nfl/' + team.split(' ').join('-').to_s + '/roster/' + year.to_s + '?sort=num')
     data.css("div.divtable.divtable-striped.divtable-mobile .tr")
   end
